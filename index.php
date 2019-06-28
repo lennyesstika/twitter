62c0b3e5cae825a86be315eaa2cb2797752f6926
lennyesstika <tikatceh@gmail.com>
2019-06-28 20:34:27 +0700

    twitter

---
+++ index.php
@@ -0,0 +1,31 @@
+<!doctype HTML>
+<html lang="en">
+
+<head>
+	<title>My TimeLine</title>
+	<meta charset="utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="description" content="">
+	<meta name="keywords" content="">
+	<meta name="robots" content="index,follow">
+	<link rel="stylesheet" href="css/styles.css">
+</head>
+
+<body>
+
+	<h1>Home TimeLine Lenny Estika Sari </h1>
+
+
+	<?php include_once('twitter.php'); ?>
+
+	<footer>
+		<a href="https://github.com/kmaida/twitter-timeline-php">twitter-timeline-php</a> on <a href="http://github.com">GitHub</a><br>
+		GNU Public License
+	</footer>
+
+	<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+	<script type="text/javascript" src="//platform.twitter.com/widgets.js"></script>
+	<script type="text/javascript" src="js/twitter.js"></script>
+</body>
+
+</html>
